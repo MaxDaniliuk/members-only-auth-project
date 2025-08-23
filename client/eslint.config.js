fc80e90c1c439600b22a46235393fc4b46cc71d2
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2025-08-23 22:19:54 +0300

    Set up the client side of the project

---
+++ client/eslint.config.js
@@ -0,0 +1,53 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'error',
+      'no-console': 'off',
+      'func-names': 'off',
+      'no-constant-condition': 'off',
+      'no-process-exit': 'off',
+      'object-shorthand': 'off',
+      'class-methods-use-this': 'off',
+      'no-plusplus': 'off',
+      'no-else-return': 'off',
+      'consistent-return': 'off',
+      'no-undef': 'off',
+      'no-use-before-define': 'off',
+    },
+  },
+]);
