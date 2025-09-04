7a794522b72e52a285965a2a3406e4f1e216366e
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2025-09-04 20:25:51 +0300

    Create a signup form and validate user inputs

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,54 +1,396 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import Button from '../components/Button';
 
 export default function Signup() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    fullname: '',
+    email: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [errors, setErrors] = useState({
+    fullname: null,
+    email: null,
+    username: null,
+    password: null,
+    confirmPassword: null,
+    generalMessage: null,
+  });
+
+  function confirmFieldsValidity(latestFormData) {
+    const emailValid = checkEmailValidation(latestFormData.email);
+
+    const passwordMatch =
+      latestFormData.password === latestFormData.confirmPassword &&
+      latestFormData.password.length >= 3;
+
+    const allFielsFilled = Object.values(latestFormData).every(
+      field => field.trim() !== '',
+    );
+
+    const fieldsValid =
+      allFielsFilled && latestFormData.username.trim().length >= 3;
+
+    const isValid = fieldsValid && passwordMatch && emailValid;
+
+    setIsDisabled(!isValid);
+  }
+
+  function checkEmailValidation(emailInput) {
+    const regex =
+      /^[A-Za-z0-9](?:[A-Za-z0-9._+-]*[A-Za-z0-9])?@[A-Za-z0-9-]+(?:\.[A-Za-z0-9-]+)+$/;
+    const isValid = regex.test(emailInput);
+    return isValid;
+  }
+
+  function validateEmailOnChange(e) {
+    const email = e.target.value.trim();
+    const fieldName = e.target.name;
+    const isValid = checkEmailValidation(email);
+
+    setFormData(prevState => {
+      const updatedField = { ...prevState, [fieldName]: email };
+
+      let errorMessage = null;
+      if (email === '') {
+        errorMessage = 'The field is required.';
+      } else if (!isValid) {
+        errorMessage = 'Enter a valid email address.';
+      }
+
+      const updatedErrors = { ...errors, [fieldName]: errorMessage };
+      setErrors(updatedErrors);
+
+      confirmFieldsValidity(updatedField);
+
+      return updatedField;
+    });
+  }
+
+  function validatePasswordMatch(updatedFormData = formData) {
+    if (
+      updatedFormData.password !== '' &&
+      updatedFormData.confirmPassword !== '' &&
+      updatedFormData.password === updatedFormData.confirmPassword
+    ) {
+      if (updatedFormData.password.length >= 3) {
+        setErrors(prevState => ({
+          ...prevState,
+          password: null,
+          confirmPassword: null,
+        }));
+      } else {
+        setErrors(prevState => ({
+          ...prevState,
+          confirmPassword: null,
+        }));
+      }
+    }
+  }
+
+  function validatePasswordOnChange(e) {
+    const passwordValue = e.target.value;
+    const passwordFieldName = e.target.name;
+    setFormData(prevState => {
+      const updated = {
+        ...prevState,
+        [passwordFieldName]: passwordValue,
+      };
+
+      let passwordError;
+      if (passwordValue === '') {
+        passwordError = 'Create a password at least 3 characters long.';
+      } else if (passwordValue.length === 1) {
+        passwordError =
+          'This password is too easy to guess. Please create a new one.';
+      } else if (passwordValue.length < 3) {
+        passwordError = 'Create a password at least 3 characters long.';
+      } else {
+        passwordError = null;
+      }
+
+      let confirmPasswordError;
+      if (
+        (passwordValue === '' && updated.confirmPassword !== '') ||
+        (passwordValue !== '' &&
+          updated.confirmPassword !== '' &&
+          passwordValue !== updated.confirmPassword)
+      ) {
+        confirmPasswordError = 'Passwords do not match.';
+      } else if (
+        passwordValue === '' &&
+        updated.confirmPassword === '' &&
+        errors.confirmPassword === 'Please confirm the passowrd.'
+      ) {
+        confirmPasswordError = null;
+      }
+
+      setErrors(prevState => ({
+        ...prevState,
+        [passwordFieldName]: passwordError,
+        confirmPassword: confirmPasswordError,
+      }));
+
+      validatePasswordMatch(updated);
+      confirmFieldsValidity(updated);
+
+      return updated;
+    });
+  }
+
+  function validatePasswordConfirmationOnChange(e) {
+    const confirmPasswordValue = e.target.value;
+    const confirmPasswordFieldName = e.target.name;
+    setFormData(prevState => {
+      const updated = {
+        ...prevState,
+        [confirmPasswordFieldName]: confirmPasswordValue,
+      };
+
+      let confirmPasswordError;
+      if (updated.password !== '' && confirmPasswordValue === '') {
+        confirmPasswordError = 'Please confirm the passowrd.';
+      } else if (
+        confirmPasswordValue !== '' &&
+        confirmPasswordValue !== updated.password
+      ) {
+        confirmPasswordError = 'Passwords do not match.';
+      } else {
+        confirmPasswordError = null;
+      }
+
+      setErrors(prevState => ({
+        ...prevState,
+        [confirmPasswordFieldName]: confirmPasswordError,
+      }));
+
+      validatePasswordMatch(updated);
+      confirmFieldsValidity(updated);
+
+      return updated;
+    });
+  }
+
+  function validatePasswordConfirmationOnBlur(e) {
+    const confirmPasswordValue = e.target.value.trim();
+    const confirmPasswordFieldName = e.target.name;
+    if (formData.password !== '' && confirmPasswordValue === '') {
+      setErrors(prevState => ({
+        ...prevState,
+        [confirmPasswordFieldName]: 'Please confirm the passowrd.',
+      }));
+    } else if (formData.password === '' && confirmPasswordValue === '') {
+      setErrors(prevState => ({
+        ...prevState,
+        [confirmPasswordFieldName]: null,
+      }));
+    }
+  }
+
+  function validateFullnameOnChange(e) {
+    const fullNameValue = e.target.value.trim();
+    const fieldName = e.target.name;
+    setFormData(prevState => {
+      const updatedField = { ...prevState, [fieldName]: fullNameValue };
+
+      const updatedErrors = {
+        ...errors,
+        [fieldName]: fullNameValue === '' ? 'The field is required.' : null,
+      };
+
+      setErrors(updatedErrors);
+
+      confirmFieldsValidity(updatedField);
+
+      return updatedField;
+    });
+  }
+
+  function validateUsernameOnChange(e) {
+    const usernameField = e.target.value.trim();
+    const fieldName = e.target.name;
+
+    setFormData(prevState => {
+      const updatedField = { ...prevState, [fieldName]: usernameField };
+
+      let errorMessage = null;
+      if (usernameField === '') {
+        errorMessage = 'The field is required.';
+      } else if (usernameField.length < 3 && usernameField.length > 0) {
+        errorMessage = 'Username should contain at least 3 characters.';
+      }
+
+      const updatedErrors = { ...errors, [fieldName]: errorMessage };
+      setErrors(updatedErrors);
+
+      confirmFieldsValidity(updatedField);
+
+      return updatedField;
+    });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const res = await fetch('/api/user/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+
+      let data;
+      try {
+        data = await res.json();
+      } catch (error) {
+        setErrors({
+          ...errors,
+          generalMessage:
+            'Unexpected response from server. Please try again later.',
+        });
+        return;
+      }
+
+      if (!res.ok) {
+        if (data?.errors) {
+          setErrors(data.errors);
+        } else {
+          setErrors({
+            ...errors,
+            generalMessage:
+              data?.message ||
+              'Unexpected server response. Please try again later.',
+          });
+        }
+        setIsDisabled(true);
+        return;
+      }
+
+      console.log('Form submitted successfully!');
+      navigate('/login');
+    } catch (error) {
+      setErrors({
+        ...errors,
+        generalMessage:
+          error.message ||
+          'Could not connect to the server. Please check your internet connection.',
+      });
+    }
+  }
+
   return (
     <section className="signup-post-page">
       <div className="form-wrapper">
-        <form autocomplete="false" action="" method="" id="form-signup">
+        <form
+          autoComplete="off"
+          action="/api/user/signup"
+          method="POST"
+          id="form-signup"
+          onSubmit={e => handleSubmit(e)}
+        >
           <div className="welcome-block">
             <h1>Create a new account</h1>
             <p>It’s quick and easy.</p>
           </div>
+
           <input
-            autocomplete="false"
+            className={errors.fullname ? 'invalid' : ''}
+            autoComplete="off"
             type="text"
             name="fullname"
             id="signup-fullname"
             placeholder="Full Name"
+            onChange={e => validateFullnameOnChange(e)}
           />
+          <span
+            className={
+              errors.fullname ? 'error-wrapper invalid' : 'error-wrapper'
+            }
+          >
+            {errors.fullname}
+          </span>
           <input
-            autoComplete="false"
-            type="email"
+            className={errors.email ? 'invalid' : ''}
+            autoComplete="off"
+            type="text"
             name="email"
             id="signup-email"
             placeholder="Email"
+            onChange={e => validateEmailOnChange(e)}
           />
+
+          <span
+            className={errors.email ? 'error-wrapper invalid' : 'error-wrapper'}
+          >
+            {errors.email}
+          </span>
+
           <input
-            autocomplete="false"
+            className={errors.username ? 'invalid' : ''}
+            autoComplete="off"
             type="text"
             name="username"
             id="signup-username"
             placeholder="Username"
+            onChange={e => validateUsernameOnChange(e)}
           />
+
+          <span
+            className={
+              errors.username ? 'error-wrapper invalid' : 'error-wrapper'
+            }
+          >
+            {errors.username}
+          </span>
+
           <input
-            autocomplete="false"
+            className={errors.password ? 'invalid' : ''}
+            autoComplete="off"
             type="password"
             name="password"
             id="signup-password"
             placeholder="Password"
-          />
-          <input
-            autocomplete="false"
-            type="password"
-            name="confirmpassword"
-            id="signup-confirm-password"
-            placeholder="Confirm password"
+            onChange={e => validatePasswordOnChange(e)}
           />
 
-          <Button specClass={'signup'} disabled={true}>
+          <span
+            className={
+              errors.password ? 'error-wrapper invalid' : 'error-wrapper'
+            }
+          >
+            {errors.password}
+          </span>
+
+          <input
+            className={errors.confirmPassword ? 'invalid' : ''}
+            autoComplete="off"
+            type="password"
+            name="confirmPassword"
+            id="signup-confirm-password"
+            placeholder="Confirm password"
+            onChange={e => validatePasswordConfirmationOnChange(e)}
+            onBlur={e => validatePasswordConfirmationOnBlur(e)}
+          />
+
+          <span
+            className={
+              errors.confirmPassword ? 'error-wrapper invalid' : 'error-wrapper'
+            }
+          >
+            {errors.confirmPassword}
+          </span>
+
+          <Button specClass={'signup'} disabled={isDisabled}>
             Sign up
           </Button>
+          {errors.generalMessage && (
+            <p className="general-err-msg">{errors.generalMessage}</p>
+          )}
         </form>
         <div className="redirect-block">
           <p>
