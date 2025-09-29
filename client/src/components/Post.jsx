b4f5cb87cc0cfe0ba7267022cb54c98b88b4d03a
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2025-09-29 20:30:53 +0300

    Create admin only delete button for post deletion

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,7 +1,57 @@
 import { format, parseISO } from 'date-fns';
+import { useAuthContext } from '../hooks/useAuthContext';
+import { usePostsContext } from '../hooks/usePostsContext';
 import noImage from '../assets/images/noImage.jpg';
+import Trashcan from '../assets/images/trashcan.svg?react';
 
 export default function Post({ post, authorized }) {
+  const { user } = useAuthContext();
+  const { postsState, dispatch: postsDispatch } = usePostsContext();
+
+  async function deletePost() {
+    const post_id = post.post_id;
+    try {
+      const res = await fetch(`/api/delete/posts/${post_id}`, {
+        method: 'DELETE',
+        credentials: 'include',
+      });
+
+      let data;
+      try {
+        data = await res.json();
+      } catch (error) {
+        console.error(
+          'Unexpected response from server. Please try again later.',
+        );
+        return;
+      }
+      if (!res.ok) {
+        if (data?.message) {
+          console.error(data.message);
+        } else {
+          console.error(
+            'Unexpected response from server. Please try again later.',
+          );
+        }
+        return;
+      }
+
+      console.log(data.message);
+      postsDispatch({
+        type: 'SUCCESS',
+        payload: {
+          authorized,
+          posts: postsState.postsResponse.posts.filter(
+            post => post.post_id !== post_id,
+          ),
+        },
+      });
+    } catch (error) {
+      console.error('Unexpected server response. Please try again later.');
+      return;
+    }
+  }
+
   return (
     <li className="post-wrapper" data-post-id={post.post_id}>
       <div>
@@ -21,7 +71,11 @@ export default function Post({ post, authorized }) {
           <p className="post-text">{post.post}</p>
         </div>
       </div>
-      <div className="trashcan"></div>
+      {user && user?.isadmin && (
+        <button type="button" className="delete-button">
+          <Trashcan className="trashcan" onClick={deletePost} />
+        </button>
+      )}
     </li>
   );
 }
