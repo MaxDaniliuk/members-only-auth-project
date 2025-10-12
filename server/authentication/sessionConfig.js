638c35e744830fc2c612107ccc59cf6d13aa1e79
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2025-10-12 12:34:00 +0300

    Use connect-pg-simple to store user session in DB

--- server/authentication/sessionConfig.js
+++ server/authentication/sessionConfig.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
+const pool = require("../db/pool");
 const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
 
 const sessionConfig = session({
+  store: new pgSession({
+    pool: pool,
+    tableName: "session",
+    createTableIfMissing: true,
+  }),
   secret: process.env.COOKIE_SECRET,
   resave: false,
   saveUninitialized: false,
